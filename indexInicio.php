f789fa4a20155717f4ab7c3a8eefa797ebf02e3c
Gabriel Romero Moscoso <grm-5@hotmail.com>
2023-07-27 22:14:34 -0600

    Add files via upload

---
+++ indexInicio.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE= edge">
+        <meta name="viewport" content="width=device-whidth, initial-scale=1.0">
+        <title>COMPLEJO CULTURAL</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light" >
+            <div class="container-fluid bg-danger text-white fixed-top" >
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">
+                        <img src="imagenes/e278d97d-c8bf-4f37-b6e0-a3204d6f34c0.jpg" alt="logo" width="150px">
+                    </a>
+                  </li>
+              <a class="navbar-brand text-white "  href="#">COMPLEJO<br><U>CULTURAL</U><br>SIGLO XXI</a>
+              
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent" >
+        
+                <form class="d-flex container-xl" role="search">
+                  <input class="form-control me-2 " type="search" placeholder="Buscar" aria-label="Buscar">
+                  <button class="btn btn-outline-success text-white" type="submit">Buscar</button>
+                </form>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#" >
+                        <img src="imagenes/456212.png" width="40px">
+                    </a>
+                  </li>
+              </div>
+            </div>
+          </nav>
+</head>
+<body>
+<br></br>
+<br></br>
+<br></br>
+<sub>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid container-sm sticky-top bg-secondary text-white">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active h3 text-white" aria-current="page" href="#">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link h3 text-white" href="cartelera/cartelera.html">Cartelera</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link h3 text-white" href="servicios/servicios.html">Servicios</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link h3 text-white" href="ubicacion/index.html">Ubicacion</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link h3 text-white" href="somos/index.php">Quienes somos</a>
+        </li>
+      </ul>
+   </div>
+ </div>
+   </nav>
+</sub>
+
+   
+ <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+  <div class="carousel-inner container-sm">
+    <div class="carousel-item active">
+      <img src="imagenes/d1.jpg" class="d-block w-100" alt="..." width="800" height="500">
+    </div>
+    <div class="carousel-item">
+      <img src="imagenes/d2.jpg" class="d-block w-100" alt="..." width="800" height="500">
+    </div>
+    <div class="carousel-item">
+      <img src="imagenes/d4.jpg" class="d-block w-100" alt="..." width="800" height="500">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+  
+ </div>
+ </section>
+<div class="p-3 mb-2 bg-light text-dark">      
+   <h1>Complejo Cultural Siglo XXI12</h1>
+      
+</div>
+
+
+<div class="card-group container-lg">
+  <div class="card ">
+    <img src="imagenes/456212.png" class="img-fluid" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="imagenes/456212.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="imagenes/456212.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="imagenes/456212.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="imagenes/456212.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+</div>
+<div class="card-group container-lg">
+  <div class="card ">
+    <img src="imagenes/456212.png" class="img-fluid" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="imagenes/456212.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="imagenes/456212.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="imagenes/456212.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="imagenes/456212.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+</div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+<footer>
+      <div class="container-fluid fixed-bottom ">
+        
+        <div class="row p-5 bg-danger text-white">
+          <div class="col-xs-12 col-md-6 col-lg-3">
+            <p div class="h8">Complejo Cultural siglo XXI| Todos los derechos reservados © 2023</p>
+            <a class="text-white" href="#"> </a>
+
+          </div>
+
+          <div class="col-xs-12 col-md-6 col-lg-3">
+            <a div class="text-white " href="#">Politica de privacidad</a>
+          </div>
+
+          <div class="col-xs-12 col-md-6 col-lg-3">
+            <a div class="text-white " href="#">Aviso Legal</a>
+          </div>
+
+          <div class="col-xs-12 col-md-6 col-lg-3">
+            <a href="https://www.facebook.com/CCUBUAP/?locale=es_LA" style="text-decoration:none">
+              <img src="imagenes/logo-facebook.svg"  width="40px"> 
+            </a>  
+            <a href="https://www.facebook.com/CCUBUAP/?locale=es_LA" style="text-decoration:none">
+              <img src="imagenes/logo-whatsapp.svg"  width="40px"> 
+            </a>  
+            <a href="https://twitter.com/i/flow/login?redirect_after_login=%2Fccubuap%3Flang%3Des" style="text-decoration:none">
+              <img src="imagenes/logo-twitter.svg"  width="40px"> 
+            </a>
+            <a href="https://www.youtube.com/watch?v=QWm4wYidIoY" style="text-decoration:none">
+              <img src="imagenes/logo-youtube.svg"  width="40px">   
+            </a>   
+          </div>
+          
+
+        </div>
+
+      </div>
+</footer>
+</html>
---
+++ somos/index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE= edge">
+        <meta name="viewport" content="width=device-whidth, initial-scale=1.0">
+        <title>COMPLEJO CULTURAL</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light" >
+            <div class="container-fluid bg-danger text-white fixed-top" >
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">
+                        <img src="imagenes/e278d97d-c8bf-4f37-b6e0-a3204d6f34c0.jpg" alt="logo" width="150px">
+                    </a>
+                  </li>
+              <a class="navbar-brand text-white "  href="#">COMPLEJO<br><U>CULTURAL</U><br>SIGLO XXI</a>
+              
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent" >
+        
+                <form class="d-flex container-xl" role="search">
+                  <input class="form-control me-2 " type="search" placeholder="Buscar" aria-label="Buscar">
+                  <button class="btn btn-outline-success text-white" type="submit">Buscar</button>
+                </form>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#" >
+                        <img src="imagenes/456212.png" width="40px">
+                    </a>
+                  </li>
+              </div>
+            </div>
+          </nav>
+</head>
+<body>
+<br></br>
+<br></br>
+<br></br>
+<sub>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid container-sm sticky-top bg-secondary text-white">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active h3 text-white" href="indexInicio.php">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link h3 text-white" href="#">Cartelera</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link h3 text-white" href="servicios/servicios.html">Servicios</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link h3 text-white" href="#">Ubicacion</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link h3 text-white" href="#">Quienes somos</a>
+        </li>
+      </ul>
+   </div>
+ </div>
+   </nav>
+</sub>
+
+<div class="card bg-dark text-white container-sm">
+  <img src="imagenes/d4.jpg" class="card-img" alt="..." height="350px">
+  <div class="card-img-overlay">
+    <h5 class="card-title"><center>COMPLEJO<br><U>CULTURAL</U><br>SIGLO XXI</center></h5>
+  </div>
+</div>
+
+<div class="card mb-3 container-lg" >
+  <div class="row g-0">
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Complejo Cultural Siglo XXI </h5>
+        <p class="card-text">El auditorio fue creado por el arquitecto Pedro Ramírez Vázquez e inaugurado en el 2005. Fue recientemente remodelado reemplazando La fachada que contenia una composición considerada la obra de Talavera más grande del mundo, por una gran vidriera.</p>
+        <p>Gobierno del Estado de Puebla <br>Pléyades s/n<br>Reserva Territorial Atlixcáyotl<br>CP 72560, Puebla, Puebla<br>Tels.: 222 122 11 00 ext. 3100</p>
+      </div>
+      <div>
+      <form>
+         <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Nombre</label>
+          <input type="nombre" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+          <div id="nombre" class="form-text">Escribe tu nombre completo.</div>
+          </div>
+
+          <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Correo</label>
+          <input type="correo" class="form-control" id="exampleInputPassword1">
+          </div>
+
+          <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Telefono</label>
+          <input type="telefono" class="form-control" id="exampleInputPassword1">
+          </div>
+
+          <select class="form-select" aria-label="Default select example">
+           <option selected>Servicio de intereses</option>
+           <option value="1">Cine</option>
+           <option value="2">Auditorio</option>
+           <option value="3">Teatro</option>
+          </select>
+
+          <div class="mb-3 form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1">
+          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+          </div>
+          <button type="submit" class="btn btn-primary">Enviar</button>
+      </form>
+      </div>
+    </div>
+    <div class="col-md-4">
+    <a href="https://www.google.com/maps/place/TEATRO+COMPLEJO+CULTURAL+UNIVERSITARIO/@19.0198351,-98.2408217,15z/data=!4m2!3m1!1s0x0:0xdbc36c75f15ca8d7?sa=X&ved=2ahUKEwjB89ei56qAAxWklmoFHd51DLYQ_BJ6BAhREAA&hl=es&ved=2ahUKEwjB89ei56qAAxWklmoFHd51DLYQ_BJ6BAhcEAg" style="text-decoration:none">
+      <img src="imagenes/ubi.png"  alt="..." height="250px">
+    </a>
+    </div>
+  </div>
+</div>
+        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<br></br>
+<br></br>
+<br></br>
+</body>
+<footer>
+      <div class="container-fluid fixed-bottom ">
+        
+        <div class="row p-5 bg-danger text-white">
+          <div class="col-xs-12 col-md-6 col-lg-3">
+            <p div class="h8">Complejo Cultural siglo XXI| Todos los derechos reservados © 2023</p>
+            <a class="text-white" href="#"> </a>
+
+          </div>
+
+          <div class="col-xs-12 col-md-6 col-lg-3">
+            <a div class="text-white " href="#">Politica de privacidad</a>
+          </div>
+
+          <div class="col-xs-12 col-md-6 col-lg-3">
+            <a div class="text-white " href="#">Aviso Legal</a>
+          </div>
+
+          <div class="col-xs-12 col-md-6 col-lg-3">
+            <a href="https://www.facebook.com/CCUBUAP/?locale=es_LA" style="text-decoration:none">
+              <img src="imagenes/logo-facebook.svg"  width="40px"> 
+            </a>  
+            <a href="https://www.facebook.com/CCUBUAP/?locale=es_LA" style="text-decoration:none">
+              <img src="imagenes/logo-whatsapp.svg"  width="40px"> 
+            </a>  
+            <a href="https://twitter.com/i/flow/login?redirect_after_login=%2Fccubuap%3Flang%3Des" style="text-decoration:none">
+              <img src="imagenes/logo-twitter.svg"  width="40px"> 
+            </a>
+            <a href="https://www.youtube.com/watch?v=QWm4wYidIoY" style="text-decoration:none">
+              <img src="imagenes/logo-youtube.svg"  width="40px">   
+            </a>   
+          </div>
+          
+
+        </div>
+
+      </div>
+</footer>
+</html>
