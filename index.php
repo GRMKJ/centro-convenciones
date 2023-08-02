50cebf51be3190638e54de9b415d9331da87c3b5
Gabriel Romero <grm-5@hotmail.com>
2023-08-02 16:44:21 -0600

    Esqueleto del Index terminado

---
+++ index.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport">
+  <title>Complejo Cultural Siglo XXI - Inicio</title>
+  <link rel="stylesheet" href="css/custom.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script></head>
+<body>
+  <!-- Header -->
+  <header class="w-100">
+    <nav class="navbar navbar-expand-lg ps-5-lg pe-5-lg">
+        <div class="container-fluid">
+            <a class="navbar-brand me-auto me-lg-5" aria-current="page" href="">
+              <img  id="logo" src="identidad/CULTURA1White.png" class="img-fluid float-end w-auto" alt="logo">
+            </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span><i class="bi bi-list"></i></span>
+          </button>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#userSupportedContent" aria-controls="userSupportedContent" aria-expanded="false" aria-label="Toggle User">
+            <span class="navbar-toggler"><i class="bi bi-person-fill"></i></span>
+          </button>
+          <div class="collapse navbar-collapse ms-2" id="navbarSupportedContent">
+            <div class="container" id="navbar">
+              <form class="d-flex mt-3 w-100" role="search">
+                <input class="form-control me-2" type="search" placeholder="Buscar en Cartelera" aria-label="Buscar">
+                <button class="btn text-white" type="submit"><i class="bi bi-search"></i></button>
+              </form>
+              <div class="container rounded" id="navbart">
+                <ul class="nav nav-pills nav-fill justify-content-center me-auto mt-3 mb-3 mb-lg-0">
+                  <li class="nav-item m-0">
+                    <a class="nav-link active align-middle text-white" aria-current="page" href="#">Inicio</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link align-middle text-white" href="cartelera/cartelera.html">Cartelera</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link align-middle text-white" href="servicios/servicios.html">Servicios</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link align-middle text-white" href="ubicacion/index.html">Ubicacion</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link align-middle text-white" href="somos/index.php">Quienes somos</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="collapse navbar-collapse ms-2" id="userSupportedContent">
+            <div class="container" id="navbar">
+                <div class="container" id="navbar">
+                <ul class="navbar-nav justify-content-center me-auto mt-3 mb-2 mb-lg-0">
+                  <li class="nav-item m-0">
+                    <a class="nav-link align-middle text-white" href="">Iniciar Sesion</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link align-middle  text-white" href="">Registrarse</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="navbar-brand align-middle" id="userslong"> 
+            <div class="btn-group ms-lg-5 mt-1">
+              <button id="useraccess" type="button" class="btn btn-lg dropdown-toggle " data-bs-toggle="dropdown" aria-expanded="false">
+                <i class="bi bi-person-fill" ></i>
+              </button>
+              <ul id="useraccesslist" class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Iniciar Sesión</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Registrarse</a></li>
+              </ul>
+            </div>
+          </div> 
+        </div>
+    </nav>
+  </header>
+  <!-- Carrusel -->
+  <section class="w-100">
+    <div class="container-fluid overflow-hidden">
+      <div id="carouselExampleFade" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="imagenes/d1.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="imagenes/d2.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="imagenes/d4.jpg" class="d-block w-100" alt="...">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  
+  <div class="card-group container-lg">
+    <div class="card ">
+      <img src="imagenes/456212.png" class="img-fluid" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+    </div>
+    <div class="card">
+      <img src="imagenes/456212.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h>
+        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+    </div>
+    <div class="card">
+      <img src="imagenes/456212.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+    </div>
+    <div class="card">
+      <img src="imagenes/456212.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+    </div>
+    <div class="card">
+      <img src="imagenes/456212.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+    </div>
+  </div>
+  <div class="card-group container-lg">
+    <div class="card ">
+      <img src="imagenes/456212.png" class="img-fluid" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+    </div>
+    <div class="card">
+      <img src="imagenes/456212.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h>
+        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+    </div>
+    <div class="card">
+      <img src="imagenes/456212.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+    </div>
+    <div class="card">
+      <img src="imagenes/456212.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+    </div>
+    <div class="card">
+      <img src="imagenes/456212.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+    </div>
+  </div>
+</body>
+<footer>
+      <div class="container-fluid">
+        
+        <div class="row p-5 bg-danger text-white">
+          <div class="col-xs-12 col-md-6 col-lg-3">
+            <p div class="h8">Complejo Cultural siglo XXI| Todos los derechos reservados © 2023</p>
+            <a class="text-white" href="#"> </a>
+
+          </div>
+
+          <div class="col-xs-12 col-md-6 col-lg-3">
+            <a div class="text-white " href="#">Politica de privacidad</a>
+          </div>
+
+          <div class="col-xs-12 col-md-6 col-lg-3">
+            <a div class="text-white " href="#">Aviso Legal</a>
+          </div>
+
+          <div class="col-xs-12 col-md-6 col-lg-3">
+            <a href="https://www.facebook.com/CCUBUAP/?locale=es_LA" style="text-decoration:none">
+              <img src="imagenes/logo-facebook.svg"  width="40px"> 
+            </a>  
+            <a href="https://www.facebook.com/CCUBUAP/?locale=es_LA" style="text-decoration:none">
+              <img src="imagenes/logo-whatsapp.svg"  width="40px"> 
+            </a>  
+            <a href="https://twitter.com/i/flow/login?redirect_after_login=%2Fccubuap%3Flang%3Des" style="text-decoration:none">
+              <img src="imagenes/logo-twitter.svg"  width="40px"> 
+            </a>
+            <a href="https://www.youtube.com/watch?v=QWm4wYidIoY" style="text-decoration:none">
+              <img src="imagenes/logo-youtube.svg"  width="40px">   
+            </a>   
+          </div>
+          
+
+        </div>
+
+      </div>
+</footer>
+</body>
+</html>
