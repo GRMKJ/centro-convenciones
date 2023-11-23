64c634d66b88947f6809bf56c9beafc844e90f91
Diego Ramirez Tome <150141101+DiegoRamtom5@users.noreply.github.com>
2023-11-23 13:58:59 +0000

    Documentacion

--- api/class/Lista.php
+++ api/class/Lista.php
@@ -6,96 +6,108 @@ class Lista
     private $root;
     private $length;
 
-    public function __Construct($valor=null){   // 
-        if ($valor != null) {   // new Lista(5)  lista  [ (5)>null  ]
-            $this->root = new Nodo($valor);  //          root^ 
-            $this->length = 1;
-        } else {               // new Lista()     
-            $this->root  = null;                // lista [ null ]
-            $this->length=0;                  //          root^
-        }
+    /**
+* Constructor de la clase.
+* @param mixed $valor Valor opcional para el nodo inicial de la lista.
+*/
+public function __Construct($valor=null){   
+    if ($valor != null) {   
+        $this->root = new Nodo($valor);   
+        $this->length = 1;
+    } else {                   
+        $this->root  = null;               
+        $this->length=0;                  
     }
+}
 
     public function getLength(){
         return $this->length;
     }
 
     public function agregarInicio($valor){
-        /*
-        if ($this->root == null)       new Lista(); 
-        {
-          $this->root = new Nodo($valor);
-        }
-        else                           new Lista(5);   3      4>5
-        {
-          $nodoNuevo = new Nodo($valor, $this->root); (3)>(4)>(5)>
-          $this->root = $nodoNuevo;                  root>(3)>(4)>(5)
-        }
-        */                                                                     //root >  (3)>(4)
         $this->root == null ? $this->root = new Nodo($valor) : $this->root = new Nodo($valor, $this->root);
         $this->length++;
     }
 
-    public function agregarIndice($valor, $indice){
-        if ($this->root == null)
-        {
-          $this->agregarInicio($valor);
-        }
-        else
-        {
-          $nodoEncontrado = $this->buscarIndice($indice-1);
-          if ( $nodoEncontrado != null ){
-            /*
-             $nuevo = new Nodo($valor);
-             $nuevo->setSig($nodoEncontrado->getSig());
-             $nodoEncontrado->setSig($nuevo);
-            */
+    /**
+* Agrega un nodo con un valor en un índice específico de la lista.
+* Si la lista está vacía, agrega el nodo como primer elemento.
+* @param mixed $valor Valor del nuevo nodo.
+* @param int $indice Índice en el que se va a agregar el nodo.
+*/
+public function agregarIndice($valor, $indice){
+    if ($this->root == null)
+    {
+        $this->agregarInicio($valor);
+    }
+    else
+    {
+        $nodoEncontrado = $this->buscarIndice($indice-1);
+        if ( $nodoEncontrado != null ){
+
             $nodoEncontrado->setSig( new Nodo($valor, $nodoEncontrado->getSig()) );
             $this->length++;
-          }
         }
     }
+}
 
-    public function agregarUltimo($valor){
-        $nodoEncontrado = $this->buscarIndice($this->length - 1);
-        if ( $nodoEncontrado != null ){
-            $nodoEncontrado->setSig( new Nodo($valor) );
-            $this->length++;
+    /**
+* Agrega un nodo con un valor al final de la lista.
+* Si la lista está vacía, agrega el nodo como primer elemento.
+* @param mixed $valor Valor del nuevo nodo.
+*/
+public function agregarUltimo($valor){
+    $nodoEncontrado = $this->buscarIndice($this->length - 1);
+    if ( $nodoEncontrado != null ){
+        $nodoEncontrado->setSig( new Nodo($valor) );
+        $this->length++;
+    }
+    else
+    {
+      $this->agregarInicio($valor);
+    }
+}
+   /**
+* Busca y devuelve el nodo de la lista en el índice especificado.
+* @param int $indice Índice del nodo a buscar.
+* @return Nodo|null El nodo encontrado en el índice o null si no es válido.
+*/
+public function buscarIndice($indice){
+    if ( $this->length > 0 && ($indice >=0 && $indice < $this->length) ){
+        $ap = $this->root;          //     root > (3) < ap
+        $cont = 0;
+        while($ap->getSig() != null && $cont < $indice){
+            $ap = $ap->getSig();
+            $cont++;
         }
-        else
-        {
-          $this->agregarInicio($valor);
+        if ($cont == $indice){
+            return $ap;
         }
     }
+    return null;
+}
 
-    public function buscarIndice($indice){
-        if ( $this->length > 0 && ($indice >=0 && $indice < $this->length) ){
-            $ap = $this->root;          //     root > (3) < ap
-            $cont = 0;
-            while($ap->getSig() != null && $cont < $indice){
-                $ap = $ap->getSig();
-                $cont++;
-            }
-            if ($cont == $indice){
-                return $ap;
-            }
+   /**
+* Busca el valor especificado y devuelve el nodo que lo contiene.
+* @param mixed $valor Valor a buscar en los nodos de la lista.
+* @return Nodo|null El nodo que contiene el valor o null si no se encuentra.
+*/
+public function buscarValor($valor){
+    if ( $this->root != null ){
+        $ap = $this->root;
+        while ($ap->getSig() != null && $valor != $ap->getVal()){
+            $ap = $ap->getSig();
         }
-        return null;
-    }
-
-    public function buscarValor($valor){
-        if ( $this->root != null ){
-            $ap = $this->root;
-            while ($ap->getSig() != null && $valor != $ap->getVal()){
-                $ap = $ap->getSig();
-            }
-            if ($valor == $ap->getVal()){
-                return $ap;
-            }
+        if ($valor == $ap->getVal()){
+            return $ap;
         }
-        return null;
     }
+    return null;
+}
 
+       /**
+     * Elimina el primer nodo de la lista.
+     */
     public function eliminarInicio(){
         if ($this->root != null) {
             if ($this->length > 1){
@@ -110,11 +122,15 @@ class Lista
             }
         }
     }
+       /**
+    * Elimina el nodo en el índice especificado.
+    * @param int $indice Índice del nodo a eliminar.
+    */
     public function eliminarIndice($indice){ 
-        if ($this->root != null && $indice <= $this->length) { //3
+        if ($this->root != null && $indice <= $this->length) { 
             if($indice == 0){
                 $this->eliminarInicio();
-            } else {                                 // 0
+            } else {                                 
                 $nodoAnterior = $this->buscarIndice($indice-1);
                 $nodoEncontrado = $nodoAnterior->getSig();
                 if ( $nodoEncontrado ){
@@ -131,8 +147,13 @@ class Lista
             $this->eliminarIndice($this->length - 1);
         }
     }
-    
-     public function modificarIndice($valor, $indice){
+
+       /**
+    * Modifica el valor de un nodo en el índice especificado.
+    * @param mixed $valor Nuevo valor para el nodo.
+    * @param int $indice Índice del nodo a modificar.
+    */
+    public function modificarIndice($valor, $indice){
         if ($this->root != null)
         {
             $nodoEncontrado = $this->buscarIndice($indice);        
@@ -142,26 +163,34 @@ class Lista
             }
         }
     }
-    
-    public function mostrar(){
-        if ( $this->length > 0 ){
-            $ap = $this->root;
-            while($ap->getSig() != null ){
-                echo $ap->getVal()."->";
-                $ap = $ap->getSig();
-            }
+
+    /**
+* Muestra los valores de todos los nodos de la lista.
+*/
+public function mostrar(){
+    if ( $this->length > 0 ){
+        $ap = $this->root;
+        while($ap->getSig() != null ){
             echo $ap->getVal()."->";
+            $ap = $ap->getSig();
         }
-        echo "null";
+        echo $ap->getVal()."->";
     }
+    echo "null";
+}
 
-    public function sublista(int $inicio,int $fin){
-        if($this->root != null){
-            
-        }
+/**
+* Obtiene una sublista de la lista original.
+* @param int $inicio Índice de inicio de la sublista.
+* @param int $fin Índice de fin de la sublista.
+* @return Lista|null La sublista obtenida o null si no es válida.
+*/
+public function sublista(int $inicio,int $fin){
+    if($this->root != null){
+
     }
+}
 
-    
 
 }
 ?>
--- api/class/Nodo.php
+++ api/class/Nodo.php
@@ -2,33 +2,53 @@
 class Nodo{
     private $valor;
     private $siguiente;
+/**
+* Establece el valor del nodo.
+* @param mixed $valor El valor a establecer.
+*/
 
-    public function setVal($valor){
-        if($valor!=null)
-            $this->valor = $valor;
-    }
+public function setVal($valor){
+    if($valor!=null)
+        $this->valor = $valor;
+}
+/**
+* Obtiene el valor del nodo.
+* @return mixed El valor del nodo.
+*/
+public function getVal(){
+   return $this->valor;
+}
 
-    public function getVal(){
-       return $this->valor;
-    }
+/**
+* Establece la referencia al siguiente nodo.
+* @param mixed $siguiente El siguiente nodo.
+*/
+public function setSig($siguiente){
+    if( is_a( $siguiente , 'Nodo' ) || $siguiente == null )
+        $this->siguiente = $siguiente;
+}
 
-    public function setSig($siguiente){
-        if( is_a( $siguiente , 'Nodo' ) || $siguiente == null )
-            $this->siguiente = $siguiente;
-    }
+/**
+* Obtiene la referencia al siguiente nodo.
+* @return mixed El siguiente nodo.
+*/
+public function getSig(){
+    return $this->siguiente;
+}
 
-    public function getSig(){
-        return $this->siguiente;
-    }
-    
-    public function __Construct($valor, $siguiente = null) {
-        if ( $valor != null )
-            $this->setVal( $valor );
-        else
-            throw new Exception('No se permite valor null'); 
-        
-        $this->siguiente = null;
-        $this->setSig( $siguiente );
-    }
-} 
+/**
+* Constructor de la clase.
+* @param mixed $valor El valor del nodo.
+* @param mixed $siguiente El siguiente nodo.
+*/
+public function __Construct($valor, $siguiente = null) {
+    if ( $valor != null )
+        $this->setVal( $valor );
+    else
+        throw new Exception('No se permite valor null'); 
+
+    $this->siguiente = null;
+    $this->setSig( $siguiente );
+}
+}
 ?>
